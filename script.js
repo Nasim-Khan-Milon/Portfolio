fad5585bf8974e483bbb6fdf085faca3db1def12
Nasim Khan Milon <nasimkhanmilon@gmail.com>
2026-02-18 13:09:41 +0600

    Add my protfolio

---
+++ script.js
@@ -0,0 +1,88 @@
+// Scroll Reveal
+window.addEventListener("scroll", function () {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+});
+
+var typed = new Typed(".typing", {
+  strings: [
+    "Software Engineering Student",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+    "Competitive Programmer"
+  ],
+  typeSpeed: 70,
+  backSpeed: 50,
+  loop: true
+});
+
+// Dark Mode Toggle
+function toggleMode(){
+  document.body.classList.toggle("light");
+}
+
+// Particle Background
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    line_linked: { enable: true },
+  }
+});
+
+
+
+// Animated Counter
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target + "+";
+    }
+  };
+
+  updateCounter();
+});
+
+// Active Navbar Highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(a => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === "#" + current) {
+      a.classList.add("active");
+    }
+  });
+});
